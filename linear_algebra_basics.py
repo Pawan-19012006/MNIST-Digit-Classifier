bfec380f36fa7754321b1e2ea1c9001a1a84e353
Pawan Eswaran <pawaneswaran19012006@gmail.com>
2026-06-04 13:12:06 +0530

    basic usage of nn.linear model to do the forward pass

--- linear_algebra_basics.py
+++ linear_algebra_basics.py
@@ -1,7 +1,14 @@
 import torch 
+import torch.nn as nn
 
-x = torch.tensor([2.0,3.0,4.0])
-w = torch.tensor([0.5,0.2,0.8])
-b = torch.tensor(1.0)
-y = torch.dot(w,x) + b # y = w1x1 + w2x2 + w3x3 + b
-print(y)
+#We introduce this model in order to perform the forward pass
+layer = nn.Linear(
+    in_features=3,
+    out_features=2
+)
+
+x = torch.tensor([2.0,3.0,4.0]) # providing the input features, 3 ip features, as specified
+output = layer(x) # applying the model
+print(output) # getting the output of the given inputs, ie, 2 outputs as specified
+print(layer.weight)
+print(layer.bias)
