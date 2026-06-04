36d40a69de8fb27891dca4a18915ea307ce9abad
Pawan Eswaran <pawaneswaran19012006@gmail.com>
2026-06-04 12:51:23 +0530

    Workflow of Deep Learning Neural Network mimicked

--- basics.py
+++ basics.py
@@ -1,10 +1,25 @@
 import torch 
 
 x = torch.tensor(2.0) #Input feature
+y_true = torch.tensor(10.0) #Actual output
 w = torch.tensor(3.0,requires_grad=True) #weights
 b = torch.tensor(1.0,requires_grad=True) #bias
-y = w * x + b
-print(y)
-y.backward()
-print(w.grad)
-print(b.grad)
+print("Initial Weight: ",w)
+print("Initial Bias: ",b)
+y_pred = w * x + b
+loss = (y_pred - y_true)**2
+print("Prediction:", y_pred)
+print("Loss", loss)
+
+#BackPropagation
+loss.backward()
+print("Gradient of w:",w.grad)
+print("Gradient of b:",b.grad)
+
+learning_rate = 0.01
+with torch.no_grad():
+    w = w - learning_rate * w.grad #W(new) = W(old) - LearningRate * w.grad
+    b = b - learning_rate * b.grad
+
+print("Updated w: ", w)
+print("Updated b: ", b)
