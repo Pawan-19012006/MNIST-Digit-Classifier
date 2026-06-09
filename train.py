7c7fb6b1afa76ae8a7828d73a0d1277796c3d38b
Pawan Eswaran <pawaneswaran19012006@gmail.com>
2026-06-09 10:46:48 +0530

    added detailed comments

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ test_dataset = datasets.MNIST(
     transform = transforms.ToTensor()
 )
 
-train_loader = DataLoader(
+train_loader = DataLoader( #We are splitting the train data into batches each containing 32 images
     dataset=train_dataset,
     batch_size=32, #Batching of 32 images together
     shuffle=True #the order of the images will shuffle, else the labels will be stagnant and it creates learning bias
@@ -33,45 +33,45 @@ test_loader = DataLoader(
 
 class MNISTModel(nn.Module):
     def __init__(self):
-        super().__init__()
+        super().__init__() #input layer layer only passes the features, there will be no activation taking place in there, also initially the number of neurons will be equal to the number of features in the input layer, but after that it will keep reducing
         self.layer1 = nn.Linear(784,128) #Layer 1 is the first hidden layer, it has 784 input features and 128 Neurons
-        self.relu = nn.ReLU()
+        self.relu = nn.ReLU() #it is the activation function that activates the neurons and also introduces a nonlinearity
         self.layer2 = nn.Linear(128,10)#Layer 2 is the output layer, that only posesses 10 neurons, becoz each neuron represents a digit
 
-    def forward(self,x):
+    def forward(self,x): #forward function is responsible for the pathway/flow of the layers, what layer to be passed after another will be defined in here
         x = self.layer1(x)
         x = self.relu(x)
         x = self.layer2(x)
         return x
     
-model = MNISTModel()
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(
-    model.parameters(),
-    lr=0.01
+model = MNISTModel() #We use the mnistmodel which is the class we created above
+criterion = nn.CrossEntropyLoss() #it is the one used for the loss function finding
+optimizer = torch.optim.SGD( #We use the Stochastic Gradient Descent Optimizer in here
+    model.parameters(), #We feed all the parameters to it, ie the weights and the bias
+    lr=0.01 #Learning rate is constant and stable throughout the optimization in case of SGD
 )
 
-for epoch in range(5):
-    running_loss=0
-    for images,labels in train_loader:
-        images = images.view(images.shape[0],-1)
-        outputs = model(images)
-        loss = criterion(outputs,labels)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        running_loss = running_loss + loss.item()
-    epoch_loss = running_loss/len(train_loader)
+for epoch in range(5): #One epoch will contain all the forward pass, backward pass and optimization of the whole train dataset, example if there are 60000 images in dataset, we split it into 32 batches, so there will be 1875 batches alltogether, so one epoch will contain 1875 forward pass, 1875 backprop and 1875 optimizer updates
+    running_loss=0 #initiating the running loss variable
+    for images,labels in train_loader: #we fetch the images and labels from the train loader(train loader sends each batch data)
+        images = images.view(images.shape[0],-1) # Here we flatten(.view can reshape the images) the images coz, initially it will contain [batchsize,h,w,colorcode], now after flattening it will turn to [batchsize,features] so that it can be passed as the feature vector
+        outputs = model(images) #here the init function and forward function will be called automatically on the inside
+        loss = criterion(outputs,labels)#Finding the loss function by using the output labels we found above and actual labels
+        optimizer.zero_grad() #clearing the optimizer so that it does not accumulate the gradients
+        loss.backward() #backpropagation starts, here the backward function is automatically derived by pytorch using Autograd which creates the computational graph
+        optimizer.step() #.step is the one that actually updates the parameters, here is where new weights and biases are found using the formula and then are updated
+        running_loss = running_loss + loss.item() #Running loss adds the loss from each batch(loss.item gives the loss number from each batch)
+    epoch_loss = running_loss/len(train_loader) #gives the average loss for the whole epoch once the for loop gets executed, here we get runningloss/1875 since there are 1875 batches in the trainloader
    # print(f"Epoch {epoch+1}, Loss: {epoch_loss}")
 
-model.eval() #This will state that the model is in inference/evaluation mode
+model.eval() #This will state that the model is in evaluation mode, has to be specified before the testing and evaluation
 correct = 0
 total = 0
-with torch.no_grad():
+with torch.no_grad():#it states not to track and keep history of gradients
     for images, labels in test_loader:
-        images = images.view(images.shape[0],-1)
-        outputs = model(images)
-        predictions = torch.argmax(outputs,dim=1)
+        images = images.view(images.shape[0],-1) #flattening
+        outputs = model(images) #predicting
+        predictions = torch.argmax(outputs,dim=1) #when there are 32 images, each image will get an output value, we take the highest value out of it 
         correct += (predictions==labels).sum().item()
         total+=labels.size(0)
 accuracy = 100 * correct/total
